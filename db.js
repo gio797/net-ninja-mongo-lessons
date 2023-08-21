736379a63be34eb2e7e5caff88a358eac451fdf8
gio797 <g.muchaidze@yahoo.com>
2023-08-21 18:13:58 +0400

    add atlas uri

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { MongoClient } = require("mongodb");
 
 let dbConnection;
+let uri = `mongodb+srv://giomuchaidze:giorgim123@cluster0.08l2kuy.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = {
   connectToDb: (cb) => {
     // MongoClient.connect('mongodb://localhost:27017/bookstore') not worked
-    MongoClient.connect("mongodb://127.0.0.1/bookstore")
+    //MongoClient.connect("mongodb://127.0.0.1/bookstore")
+
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return cb();
